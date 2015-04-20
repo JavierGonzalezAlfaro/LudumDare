5fada69316748cdef21d8c4b345732f3bcf74772
Javier Gonzalez <javichu.12@gmail.com>
2015-04-20 17:57:34 +0100

    menu simple que lleva a las escenas

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void soloGame()
+    {
+        Application.LoadLevel("FactoryIADemo");
+    }
+
+    public void multiGame()
+    {
+        Application.LoadLevel("FactoryOnline");
+    }
+}
